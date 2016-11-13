3cb7641e9243660c6592044c77f3f46d24d768a1
liuhl <1029765111@qq.com>
2016-11-13 14:17:39 +0800

    angularjs 控制器与基本指令

---
+++ 1.x/app/README.md
@@ -0,0 +1,6 @@
+# 一个angularjs app
+
+- [学习angularJS 1.x](https://hairui219.gitbooks.io/learning_angular/content/zh/chapter03.html)
+
+
+
---
+++ 1.x/app/public/js/app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by liuhongliang on 2016/11/12.
+ */
+
+(function(){
+    var app = angular.module("App",[]);
+
+    app.controller("FirstCtrl",function($scope){
+        $scope.data ={
+            message:"这是第一个控制器",
+            onBtnClick:function(){
+                debugger;
+                alert(this.message);
+            }
+        };
+
+
+    });
+})();
